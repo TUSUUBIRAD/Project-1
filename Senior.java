97c696fbbf3b813e2624a81d521468b9d8d6e6ab
TUSUUBIRAD <tusuu.david@gmail.com>
2025-05-17 10:03:20 +0100

    Add files via upload
    initial project

---
+++ Senior.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package newcurriculum;
+
+/**addition of bases */
+ 
+
+ 
+public class Senior { 
+    
+    public static void main(String[] args) {
+        int a=8;
+               
+              int b=8;
+                      //computing the sum of a and b
+                      int sum = a+b;
+                      System.out.println(sum);
+                      
+                        
+        
+        
+    }
+
+ }
